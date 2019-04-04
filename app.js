b21c90d7e6a0a3f19ec86447d16342ceef36f589
Shivam <shvmbisht@gmail.com>
2019-04-05 01:34:55 +0530

    day 1

---
+++ app.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const app = express();
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(adminRoutes);                  
+
+app.use(shopRoutes)
+
+// Allows the request to continue to the next middleware in lin
+
+
+app.listen(3000);
