c839f743a4c833b8797259c5369837ac40d50e11
Shivam <shvmbisht@gmail.com>
2019-04-05 18:06:59 +0530

    End of day 2

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const http = require('http');
-
+const path = require('path')
 const express = require('express');
 const bodyParser = require('body-parser')
 
 const app = express();
 
-const adminRoutes = require('./routes/admin')
+const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
-app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/admin',adminRoutes);                  
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);                  
 
 app.use(shopRoutes)
 app.use((req,res,next) => {
-    res.status(404).send('<h1> PAGE NOT FOUND</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 // Allows the request to continue to the next middleware in lin
--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,19 @@
 const express = require('express');
-
+const path = require('path');
 const router = express.Router();
+const products = [];
+const rootDir = require('../util/path.js')
 
 router.get('/add-product',(req, res, next) => {
-    console.log('In the Slackmiddleware!');
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title" > <button type="submit" value="Submit" >Add Product</button></form>');
+    
+    res.sendFile(path.join(rootDir , 'views', 'add-product.html'));
 });
 
 router.post('/add-product',(req, res, next) => {
-    console.log(req.body);
+    
+    products.push({title: req.body.title});
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
