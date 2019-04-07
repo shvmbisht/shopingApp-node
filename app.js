6c240c73f7171da4a4b16ea1fdc5d99465e27a41
Shivam <shvmbisht@gmail.com>
2019-04-08 01:28:49 +0530

    templating basic

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
-const http = require('http');
-const path = require('path')
+const path = require('path');
+
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const app = express();
-app.set('view engine', 'pug');
-app.set('views', 'views');  
 
-const adminData = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);                  
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
-app.use(shopRoutes)
-app.use((req,res,next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-})
-
-// Allows the request to continue to the next middleware in lin
-
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,28 @@
-const express = require('express');
 const path = require('path');
-const router = express.Router();
-const products = [];
-const rootDir = require('../util/path.js')
 
-router.get('/add-product',(req, res, next) => {
-    
-    res.sendFile(path.join(rootDir , 'views', 'add-product.html'));
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+const products = [];
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
 
-router.post('/add-product',(req, res, next) => {
-    
-    products.push({title: req.body.title});
-    res.redirect('/');
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
 });
 
 exports.routes = router;
